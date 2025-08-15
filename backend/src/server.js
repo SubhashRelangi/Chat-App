ab34cc9306c3a6f85c3ee7296fd8d3ebb39ab77c
SubhashRelangi <subhashrelangi@gmail.com>
2025-08-15 18:03:28 +0530

    initilzing .env file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-    console.log('App is running at port 5000')
-})
+app.use(express.json());
+
+// testing route
+app.get("/", (req, res) => {
+    res.send("🚀 Server is up and running!");
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(` App is running at http://localhost:${PORT}`);
+});
