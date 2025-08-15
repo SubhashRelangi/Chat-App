3b03c6dc85e3c4d7332cf67c59361cc482504cae
SubhashRelangi <subhashrelangi@gmail.com>
2025-08-15 18:10:37 +0530

    connected to mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,34 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from 'cors';
 
 // Load environment variables
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGODB_URL;
 
+// Middleware
 app.use(express.json());
+app.use(cors());
 
-// testing route
+// Connect to MongoDB
+mongoose.connect(MONGO_URI)
+.then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch((err) => {
+    console.error("MongoDB connection error:", err);
+});
+
+// Test route
 app.get("/", (req, res) => {
-    res.send("🚀 Server is up and running!");
+    res.send("Server is up and running!");
 });
 
 // Start server
 app.listen(PORT, () => {
-    console.log(` App is running at http://localhost:${PORT}`);
+    console.log(`App is running at http://localhost:${PORT}`);
 });
